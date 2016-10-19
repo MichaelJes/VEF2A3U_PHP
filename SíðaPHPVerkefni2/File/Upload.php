c3a301975b2fe0b62603c47566f5c34f0c7ba5e4
Michael Jes Þórsson <mikkimus@simnet.is>
2016-10-19 19:59:27 +0000

    Revert "Revert "Revert "Kláraði rest"""
    This reverts commit e0b5084a8ca8493f955457f10b891c76ce9d1a8a.

--- SíðaPHPVerkefni2/File/Upload.php
+++ SíðaPHPVerkefni2/File/Upload.php
@@ -1,11 +1,6 @@
 <?php
  namespace File;
  class Upload {
- 	 protected $renameDuplicates;
- 	 protected $typeCheckingOn = true;
- 	 protected $notTrusted = ['bin', 'cgi', 'exe', 'js', 'pl', 'php', 'py', 'sh'];
- 	 protected $suffix = '.upload';
- 	 protected $newName;
  	 protected $destination;
 	 protected $max = 51200;
 	 protected $messages = [];
@@ -21,27 +16,11 @@
 	 }
 	 $this->destination = $path;
 	 }
-	public function upload($renameDuplicates = true) {
-		$this->renameDuplicates = $renameDuplicates;
+	public function upload() {
 	 	$uploaded = current($_FILES);
-	 	if (is_array($uploaded['name'])) {
- 		foreach ($uploaded['name'] as $key => $value) {
-			 $currentFile['name'] = $uploaded['name'][$key];
-			$currentFile['type'] = $uploaded['type'][$key];
-			$currentFile['tmp_name'] = $uploaded['tmp_name'][$key];
-			$currentFile['error'] = $uploaded['error'][$key];
-			$currentFile['size'] = $uploaded['size'][$key];
-			if ($this->checkFile($currentFile)) {
-			 $this->moveFile($currentFile);
-			 }
- 			}
- 		} 
- 	 	else 
- 	 	{
-		 	if ($this->checkFile($uploaded)) {
-		 		$this->moveFile($uploaded);
-		 	}
-		 }
+	 	if ($this->checkFile($uploaded)) {
+	 		$this->moveFile($uploaded);
+	 	}
 	 }
 	 protected function checkFile($file) {
 	 	$accept = true;
@@ -57,29 +36,19 @@
 		 if (!$this->checkSize($file)) {
 		 $accept = false;
 		 }
-		 if ($this->typeCheckingOn) {
-		 	if (!$this->checkType($file)) {
-		 	$accept = false;
-		 	}
+		 if (!$this->checkType($file)) {
+		 $accept = false;
 		 }
-		 if ($accept) {
-		  $this->checkName($file);
- 		 } 
  		 return  $accept;
  	}
  	protected function moveFile($file) {
- 		 $filename = isset($this->newName) ? $this->newName : $file['name'];
 		 $success = move_uploaded_file($file['tmp_name'],
-		 $this->destination . $filename);
+		 $this->destination . $file['name']);
 		 if ($success) {
-
-		 	$result = $file['name'] ."<br>" . ' Type: ' . $file['type'] ."<br>" . ' Size: ' . $file['size'] ."<br>" .' Was uploaded successfully';
-		 	if (!is_null($this->newName)) {
-			 	$result .= ', and was renamed ' . $this->newName;
-			 }
-		 	$this->messages[] = $result;
+		 $result = $file['name'] . ' was uploaded successfully';
+		 $this->messages[] = $result;
 		 } else {
- 			$this->messages[] = 'Could not upload ' . $file['name'];
+ 		$this->messages[] = 'Could not upload ' . $file['name'];
  		}		
  	}
  	public function getMessages() {
@@ -129,46 +98,6 @@
  	}
  	public function getMaxSize() {
  		return number_format($this->max/1024, 1) . ' KB';
- 	}
- 	public function allowAllTypes($suffix = true) {
-	 $this->typeCheckingOn = false;
-	 if (!$suffix) {
-		 $this->suffix = ''; // empty string
- 	 }
-	}
-	public function setMaxSize($num) {
-	 if (is_numeric($num) && $num > 0) {
-	 	$this->max = (int) $num;
-	 }
- 	}
- 	protected function checkName($file) {
-	 $this->newName = null;
-	 $nospaces = str_replace(' ', '_', $file['name']);
-	 if ($nospaces != $file['name']) {
-	 	$this->newName = $nospaces;
-	 }
-	 $extension = pathinfo($nospaces, PATHINFO_EXTENSION);
-	 if (!$this->typeCheckingOn && !empty($this->suffix)) {
-		 if (in_array($extension, $this->notTrusted) || empty($extension)) {
-		 	$this->newName = $nospaces . $this->suffix;
-		 }
-	 }
-	 if ($this->renameDuplicates) {
-		 $name = isset($this->newName) ? $this->newName : $file['name'];
-		 $existing = scandir($this->destination);
-		 if (in_array($name, $existing)) {
-		 // rename file
-		 		$basename = pathinfo($name, PATHINFO_FILENAME);
-				 $extension = pathinfo($name, PATHINFO_EXTENSION);
-				 $i = 1;
-				 do {
-				 $this->newName = $basename . '_' . $i++;
-				 if (!empty($extension)) {
-				 $this->newName .= ".$extension";
-				 }
-				 } while (in_array($this->newName, $existing));
-		 }
-	 }
  	}
 
 
