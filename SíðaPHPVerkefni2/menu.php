e4a73e3e05153b1e4f46d27ef2ae0e7816ddb7a7
Michael Jes Þórsson <mikkimus@simnet.is>
2016-11-04 10:02:11 +0000

    Byrjun á að Sýna

--- SíðaPHPVerkefni2/menu.php
+++ SíðaPHPVerkefni2/menu.php
@@ -1,8 +1,11 @@
 <?php 
 include './includes/session_timeout.php'; 
 require_once './includes/connection.php';
+$sql = 'SELECT imagename, imagetext FROM images';
+
 require 'File/Image.php';
 $dbUsers = new Image($conn);
+
 require 'File/Upload.php';
 
 
@@ -42,9 +45,9 @@ if (isset($_POST['upload'])) {
         $result = $loader->getMessages();
 
 
-
-        $Validali = $dbUsers->newImageInfo($namo,$destination.$namo,substr($namo,0,-4),1); 
-        if($Validali) {    
+        $place = substr($destination.$namo,29,29);
+        $Validali = $dbUsers->newImageInfo($namo,$place,substr($namo,0,-4),1); 
+        if($Validali) {
             print("You did it");
         }
 
@@ -59,11 +62,26 @@ if (isset($_POST['upload'])) {
 <html>
 <head>
     <meta charset="utf-8">
-    <title>The file upload area</title>
+    <title>The file upload Area</title>
 </head>
 
 <body>
     <?php
+        $resulto = $dbUsers->imagelist();
+        if ($resulto) {
+            echo "Yah";
+            ?>
+            <pre>
+            <?php
+            print_r($resulto);
+            ?>
+            </pre>
+            <?php
+        }
+        else
+        {
+            echo "No";
+        }
         // Keyrir bara ef það er búið að smella á submit 
         if (isset($result)) {
             echo '<ul>';
@@ -84,18 +102,28 @@ if (isset($_POST['upload'])) {
 	 <input type="submit" name="upload" id="upload" value="Upload">
 	 </p>
 </form>
-
 <p><a href="secretpage.php">Another secret page</a> </p>
 
 <form method="post" action="">
 	 	<input name="logout" type="submit" value="Log out">
 </form>
-<pre>
-<?php
-if (isset($_POST['upload'])) {
-  print_r($_FILES);
-}
-?>
-</pre>
+
+   <main>
+        <h2>Images of Japan</h2>
+      <p id="picCount">Displaying 1 to 6 of 8</p>
+        <div id="gallery">
+            <table id="thumbs">
+                <tr>
+                    <!--This row needs to be repeated-->
+                    <td><a href="gallery.php"><img src="images/thumbs/basin.jpg" alt="" width="80" height="54"></a></td>
+                </tr>
+                <!-- Navigation link needs to go here -->
+            </table>
+            <figure id="main_image">
+                <img src="userImg/sd2.png" alt="" width="350" height="237">
+                <figcaption>Water basin at Ryoanji temple, Kyoto</figcaption>
+            </figure>
+        </div>
+    </main>
 </body>
 </html>
