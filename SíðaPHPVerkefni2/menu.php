08c18315babc7f7bfad3d67af068cce9002bd13d
Michael Jes Þórsson <mikkimus@simnet.is>
2016-11-09 15:14:43 +0000

    Mynda Upload

--- SíðaPHPVerkefni2/menu.php
+++ SíðaPHPVerkefni2/menu.php
@@ -45,10 +45,10 @@ if (isset($_POST['upload'])) {
         $result = $loader->getMessages();
 
 
-        $place = substr($destination.$namo,29,29);
+        $place = substr($destination.$namo,29,40);
         $Validali = $dbUsers->newImageInfo($namo,$place,substr($namo,0,-4),1); 
         if($Validali) {
-            print("You did it");
+            print($place);
         }
 
 
@@ -82,6 +82,12 @@ if (isset($_POST['upload'])) {
         {
             echo "No";
         }
+        $mainImage = $resulto[3][0];
+        $PathwayToFile = $resulto[3][1];
+        print($PathwayToFile);
+        // get the dimensions of the main image
+        $imageSize = getimagesize($PathwayToFile)[3];
+
         // Keyrir bara ef það er búið að smella á submit 
         if (isset($result)) {
             echo '<ul>';
@@ -110,18 +116,27 @@ if (isset($_POST['upload'])) {
 
    <main>
         <h2>Images of Japan</h2>
+
       <p id="picCount">Displaying 1 to 6 of 8</p>
         <div id="gallery">
             <table id="thumbs">
                 <tr>
                     <!--This row needs to be repeated-->
-                    <td><a href="gallery.php"><img src="images/thumbs/basin.jpg" alt="" width="80" height="54"></a></td>
+                    <?php
+                    $last = count($resulto) - 1;
+                    foreach ($resulto as $i => $row)
+                    {
+                        $isFirst = ($i == 0);
+                        $isLast = ($i == $last);
+                    ?>
+                    <td><a href="http://tsuts.tskoli.is/2t/3108982369/PHP/<?= $resulto[$i][1]; ?>"><img src="<?= $resulto[$i][1]; ?>"" alt="<?= $resulto[$i][0]; ?>" width="80" height="54"></a></td>
+                    <?php } ?>
                 </tr>
                 <!-- Navigation link needs to go here -->
             </table>
             <figure id="main_image">
-                <img src="userImg/sd2.png" alt="" width="350" height="237">
-                <figcaption>Water basin at Ryoanji temple, Kyoto</figcaption>
+                <img src="<?= $PathwayToFile; ?>" alt="<?= $mainImage; ?>"<?= $imageSize; ?>>
+                <figcaption><?= $mainImage; ?></figcaption>
             </figure>
         </div>
     </main>
